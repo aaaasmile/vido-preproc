566ef56b195af0d4c7a95ba47216bccf7c979a01
aaaasmile <testhaus@gmx.at>
2018-12-29 15:39:15 +0100

    preview button

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,16 +1,16 @@
 console.log('Hi, custom is loaded.')
 $('.message .close')
-  .on('click', function() {
+  .on('click', function () {
     $(this)
       .closest('.message')
       //.fadeToggle() // Nota questo non è quello che si vede in semnati ui (.transition("fade")). Ma è la funzione presa da jquery senza usare semantic-2.4-2.min.js
       .transition('fade') // per usare questo fade, bisogna includere anche semantic-2.4-2.min.js (270k di roba, solo per questa funzione!!)
-    ;
+      ;
   })
-;
+  ;
 
 $('#bttextile')
-  .on('click', () =>{
+  .on('click', () => {
     console.log("Preview clicked!")
     //console.log( textile( "I am using __textile__." ) );
     //let htmlprev = textile( "I am using __textile__." )
@@ -18,3 +18,9 @@ $('#bttextile')
     let htmlprev = textile(rawtext)
     $('#preview').empty().append(htmlprev)
   });
+
+$('#bttextile')
+  .popup({
+    inline: true
+  })
+  ;
